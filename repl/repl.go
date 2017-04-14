0fd82f681a681db239ac2a8c53f7def2becf0ccd
Joseph Portaro <jsportaro@outlook.com>
2017-04-13 20:51:49 -0400

    Chapeter 1 done. On to Chapter 2...

---
+++ repl/repl.go
@@ -0,0 +1,35 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"Monkey/lexer"
+	"Monkey/token"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const prompt = ">>"
+
+//Start It begins here
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(prompt)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
