528fe6b0e663db2ceb6404b17801026777fca1a6
Joseph Portaro <jsportaro@outlook.com>
2017-04-22 12:51:06 -0400

    Added fun error printing

--- repl/repl.go
+++ repl/repl.go
@@ -11,6 +11,18 @@ import (
 )
 
 const prompt = ">>"
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 //Start It begins here
 func Start(in io.Reader, out io.Writer) {
@@ -41,6 +53,9 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
